593609e12ce39626d76e00d3d3a705bbb5c877a3
Tristan Harris <tris.dev@pumpcottage.me.uk>
2013-12-13 20:28:22 +0000

    Add workflow graph to roles admin

--- lib/workflow_viz/graph.rb
+++ lib/workflow_viz/graph.rb
@@ -2,9 +2,10 @@ module WorkflowViz::Graph
 
 	def self.draw(role, tracker)
 		rules = role == :all ? WorkflowTransition : role.workflow_rules
+		rules = rules.where(:tracker_id => tracker) unless tracker == :all
 		GraphViz::new( :G, :type => :digraph ) do |graph|
 			node = Hash.new { |h,k| h[k] = graph.add_nodes(IssueStatus.find(k).to_s) }
-			rules.where(:tracker_id => tracker).map do |transition|
+			rules.map do |transition|
 				[transition.old_status_id, transition.new_status_id]
 			end.uniq.each do |old_id, new_id|
 				graph.add_edges(node[old_id], node[new_id])
